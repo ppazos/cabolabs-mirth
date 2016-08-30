6d138a01fae1d5dafba798e9f83791c85e487bc9
Pablo Pazos <pablo.pazos@cabolabs.com>
2016-08-30 01:59:23 -0300

    Ejemplo de ejecución de código Java desde Mirth

---
+++ mirth/channels/0011_http_xml_java/java/src/paquete/Servicio.java
@@ -0,0 +1,22 @@
+package paquete;
+
+public class Servicio {
+
+   // Constructor
+   public Servicio() {}
+
+   public static String toUpper(String s)
+   {
+      return s.toUpperCase();
+   }
+   
+   public static String toLower(String s)
+   {
+      return s.toLowerCase();
+   }
+   
+   public static int size(String s)
+   {
+      return s.length();
+   }
+}
