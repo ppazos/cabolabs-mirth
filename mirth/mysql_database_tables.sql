525e729adf874b58d87a4b082ce3b7a55ec7b7ef
Pablo Pazos <pablo.pazos@cabolabs.com>
2017-06-20 03:31:11 -0300

    updated database for english course

--- mirth/mysql_database_tables.sql
+++ mirth/mysql_database_tables.sql
@@ -1,22 +1,35 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               5.6.22 - MySQL Community Server (GPL)
--- Server OS:                    Win32
--- HeidiSQL Version:             9.1.0.4867
--- --------------------------------------------------------
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.7.18)
+# Database: mirth_tests
+# Generation Time: 2017-06-20 06:27:22 +0000
+# ************************************************************
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 -- Dumping database structure for mirth_tests
 CREATE DATABASE IF NOT EXISTS `mirth_tests` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `mirth_tests`;
 
 
--- Dumping structure for table mirth_tests.cda_logs
-CREATE TABLE IF NOT EXISTS `cda_logs` (
+# Dump of table cda_logs
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `cda_logs`;
+
+CREATE TABLE `cda_logs` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `timestamp` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `fecha_documento` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
@@ -26,21 +39,43 @@ CREATE TABLE IF NOT EXISTS `cda_logs` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
 
+# Dump of table pacientes
+# ------------------------------------------------------------
 
--- Dumping structure for table mirth_tests.pacientes
-CREATE TABLE IF NOT EXISTS `pacientes` (
-	`id` BIGINT(20) NOT NULL AUTO_INCREMENT,
-	`nombre` VARCHAR(50) NOT NULL DEFAULT '0',
-	`apellido` VARCHAR(50) NOT NULL DEFAULT '0',
-	`nacimiento` DATE NOT NULL DEFAULT '0000-00-00',
-	`sexo` VARCHAR(50) NOT NULL DEFAULT '0',
-	`procesado` TINYINT(4) NOT NULL DEFAULT '0',
-	PRIMARY KEY (`id`)
+DROP TABLE IF EXISTS `pacientes`;
+
+CREATE TABLE `pacientes` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL DEFAULT '''''',
+  `apellido` varchar(50) DEFAULT NULL,
+  `nacimiento` date DEFAULT NULL,
+  `sexo` varchar(50) DEFAULT NULL,
+  `procesado` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+
+# Dump of table patients
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `patients`;
+
+CREATE TABLE `patients` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL DEFAULT '''''',
+  `lastname` varchar(50) DEFAULT NULL,
+  `dob` date DEFAULT NULL,
+  `sex` varchar(50) DEFAULT NULL,
+  `processed` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
